56177ae78d9ecd3da9dc9fb693d77710c630dffb
Rishabh Rawat <94976245+syedasif05@users.noreply.github.com>
2024-04-21 15:54:50 +0530

    backend auth

--- src/components/AuthComponents/Signup.jsx
+++ src/components/AuthComponents/Signup.jsx
@@ -1,68 +1,120 @@
-import React from 'react';
-import { Button } from "react-bootstrap";
+import React, { useState } from 'react';
+import { Button, Modal } from "react-bootstrap";
 import { Link } from 'react-router-dom';
 import './Signup.css';
-
-
 import signupImage from '../../Images/signup-pic.svg';
-import {
-    MDBContainer,
-    MDBCol,
-    MDBRow,
-    MDBBtn,
-    MDBIcon,
-    MDBInput,
-    MDBCheckbox
-} from 'mdb-react-ui-kit';
+import { MDBContainer, MDBCol, MDBRow, MDBInput } from 'mdb-react-ui-kit';
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth'; // Import Firebase authentication modules
+import { setDoc, doc } from 'firebase/firestore';
+import { auth, db } from "./firebase";
 
-function Signin() {
+function Signup() {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [mobile, setMobile] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [error, setError] = useState(null);
+    const [showSuccessModal, setShowSuccessModal] = useState(false);
+
+    const handleSignup = async () => {
+        // Regular expression to enforce password criteria
+        const passwordRegex = /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+        // Check if the password meets the criteria
+        if (!passwordRegex.test(password)) {
+            setError('Password must contain at least one uppercase letter, one number, one special character, and be at least 8 characters long');
+            return;
+        }
+
+        if (password !== confirmPassword) {
+            setError('Passwords do not match');
+            return;
+        }
+
+        // Check if the mobile number has exactly 10 digits
+        if (!/^\d{10}$/.test(mobile)) {
+            setError('Mobile number must have exactly 10 digits');
+            return;
+        }
+
+        createUserWithEmailAndPassword(auth, email, password)
+            .then(async () => {
+                // Signed up successfully
+                setShowSuccessModal(true);
+                const user = auth.currentUser;
+                if (user) {
+                    await setDoc(doc(db, "Users", user.uid), {
+                        email: user.email,
+                        name: name,
+                        mobile: mobile
+                    });
+                }
+            })
+            .catch((error) => {
+                // Handle errors
+                setError(error.message);
+                console.error(error);
+            });
+    };
+
+    const handleCloseSuccessModal = () => {
+        setShowSuccessModal(false);
+    };
+
     return (
         <MDBContainer fluid className="p-3 my-5">
-
             <MDBRow className="d-flex align-items-stretch justify-content-center flex-grow-1">
-
                 <MDBCol col='6' md='6' className="d-flex align-items-center justify-content-center flex-grow-1">
-                    <img src={signupImage} class="img-fluid" alt="Phone image" />
+                    <img src={signupImage} className="img-fluid" alt="Phone image" />
                 </MDBCol>
-
                 <MDBCol col='6' md='6' className="d-flex flex-column justify-content-center flex-grow-1">
-
                     <h1 className="text-center mb-4 align-items-start">Signup</h1>
-
                     <div className="mb-4">
                         <label htmlFor="name" className="form-label">Name</label>
-                        <MDBInput id="name" type="text" size="lg" />
+                        <MDBInput id="name" type="text" size="lg" value={name} onChange={(e) => setName(e.target.value)} />
                     </div>
                     <div className="mb-4">
                         <label htmlFor="email" className="form-label">Email address</label>
-                        <MDBInput id="email" type="email" size="lg" />
+                        <MDBInput id="email" type="email" size="lg" value={email} onChange={(e) => setEmail(e.target.value)} />
                     </div>
                     <div className="mb-4">
                         <label htmlFor="mobile" className="form-label">Mobile Number</label>
-                        <MDBInput id="mobile" type="tel" size="lg" />
+                        <MDBInput id="mobile" type="tel" size="lg" value={mobile} onChange={(e) => setMobile(e.target.value)} />
                     </div>
                     <div className="mb-4">
                         <label htmlFor="password" className="form-label">Password</label>
-                        <MDBInput id="password" type="password" size="lg" />
+                        <MDBInput id="password" type="password" size="lg" value={password} onChange={(e) => setPassword(e.target.value)} />
                     </div>
                     <div className="mb-4">
                         <label htmlFor="confirmPassword" className="form-label">Confirm Password</label>
-                        <MDBInput id="confirmPassword" type="password" size="lg" />
+                        <MDBInput id="confirmPassword" type="password" size="lg" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
                     </div>
-
+                    {error && <div className="alert alert-danger">{error}</div>}
                     <div className="center-the-button">
-
-                        <Button className="listing-submit-button" type="submit">
-                            Signup
-                        </Button>
-
+                        <Button className="listing-submit-button" type="button" onClick={handleSignup}>Signup</Button>
                     </div>
-
-
+                    <div className="text-center mt-3">
+                        <p>Already have an account? <Link to="/login">Sign in</Link></p>
+                    </div>
                 </MDBCol>
             </MDBRow>
+            <Modal show={showSuccessModal} onHide={handleCloseSuccessModal}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Success</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <p>Your account has been successfully created.</p>
+                    <p>Please log in with your email and password</p>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleCloseSuccessModal}>
+                        <Link to="/login"> Close </Link>
+                    </Button>
+                </Modal.Footer>
+            </Modal>
         </MDBContainer>
     );
 }
 
-export default Signin;
+export default Signup;
